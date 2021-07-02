a5870a83865281e740f9f3e3380f250344d9a3db
BeaQ17 <beaq235@gmail.com>
2021-07-03 00:52:45 +0200

    2 & 3

--- index.sql
+++ index.sql
@@ -1,1 +1,7 @@
+/* 1. Selezionare studenti nati nel 1990 */
 SELECT * FROM `students` WHERE `date_of_birth` >= '1990-01-01'
+
+/* 2. Selezionare corsi che valgono più di 10 crediti */
+SELECT * FROM `courses` WHERE `cfu` >= 10
+
+/* 3. Selezionare studenti con più di 30 anni */

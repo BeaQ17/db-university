abb19de41335d532baa769d939050f63c77d7d5a
BeaQ17 <beaq235@gmail.com>
2021-07-06 10:10:45 +0200

    aggiustato groupby

--- groupby.sql
+++ groupby.sql
@@ -5,7 +5,10 @@ SELECT COUNT(*) AS `numero_iscritti`, YEAR(`enrolment_date`) AS `anno` FROM `stu
 SELECT COUNT(*) AS `numero_insegnanti`, `office_address` FROM `teachers` GROUP BY `office_address`
 
 /* 3. Calcolare la media dei voti di ogni appello d'esame */
-SELECT AVG(vote) FROM `exam_student`
+/*SELECT AVG(vote) FROM `exam_student`*/
+SELECT AVG(`vote`) AS `media_voti`, `exam_id` FROM `exam_student` GROUP BY `exam_id`
 
 /* 4. Contare quanti corsi di laurea ci sono per ogni dipartimento */
-SELECT COUNT(id) AS `numero_dipartimenti`, `name` FROM `degrees` GROUP BY `id`
+/*SELECT COUNT(id) AS `numero_dipartimenti`, `name` FROM `degrees` GROUP BY `id`*/
+SELECT COUNT(*) AS `numero_corsi`, `department_id` AS `numero_dipertimento` FROM `degrees` GROUP BY `department_id`
+

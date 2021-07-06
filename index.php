f11f1d8cc506ebbdf728f6804cff80ef5ae6a3d3
BeaQ17 <beaq235@gmail.com>
2021-07-06 15:46:32 +0200

    #2 stabilire connessione

--- index.php
+++ index.php
@@ -1,16 +1,24 @@
 <?php
 
 #1 definire costanti per la connessione
-define("DB_SERVERNAME". "localhost");
-define("DB_PORT". "3306");
-define("DB_NAME". "db University");
-define("DB_USERNAME". "root");
-define("DB_PASSWORD". "root");
+define("DB_SERVERNAME", "localhost");
+define("DB_PORT", "3306");
+define("DB_NAME", "db University");
+define("DB_USERNAME", "root");
+define("DB_PASSWORD", "root");
+
+//var_dump(DB_NAME);
 
 #2 stabilire connessione con il database
+$connection = new mysqli(DB_SERVERNAME, DB_PORT, DB_NAME, DB_USERNAME, DB_PASSWORD);
 
+//var_dump($connection);
 
 #3 verificare connessione
+if ($connection && $connection->connect_error) {
+    echo 
+    die();
+}
 
 #4 eseguire query - se la connessione è stabilita
 

a7c4c1a35de202683f4a3e5308a58dbd671fb1d4
BeaQ17 <beaq235@gmail.com>
2021-07-06 15:32:31 +0200

    #1 costanti

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+
+#1 definire costanti per la connessione
+define("DB_SERVERNAME". "localhost");
+define("DB_PORT". "3306");
+define("DB_NAME". "db University");
+define("DB_USERNAME". "root");
+define("DB_PASSWORD". "root");
+
+#2 stabilire connessione con il database
+
+
+#3 verificare connessione
+
+#4 eseguire query - se la connessione è stabilita
+
+#5 controllare che ci siano risultati da mostrare
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+    
+</body>
+</html>
